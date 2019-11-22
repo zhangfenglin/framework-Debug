1b7ba76c0f3a8752ab230a9b9eebcd149bb6822e
zhangfenglin <zhangfenglin@apusapps.com>
2019-11-22 15:16:50 +0800

    first commit

---
+++ Example/Pods/Target Support Files/Pods-XALFrameworksDebug_Example/Pods-XALFrameworksDebug_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_XALFrameworksDebug_Example : NSObject
+@end
+@implementation PodsDummy_Pods_XALFrameworksDebug_Example
+@end
---
+++ Example/Pods/Target Support Files/XALFrameworksDebug/XALFrameworksDebug-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_XALFrameworksDebug : NSObject
+@end
+@implementation PodsDummy_XALFrameworksDebug
+@end
